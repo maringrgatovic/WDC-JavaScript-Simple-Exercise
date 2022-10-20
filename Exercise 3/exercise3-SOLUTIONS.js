8ab559e755ca19c35649388024c2f5757bd9812b
maringrgatovic <56473997+maringrgatovic@users.noreply.github.com>
2022-10-20 13:20:29 +0200

    Add files via upload

---
+++ Exercise 3/exercise3-SOLUTIONS.js
@@ -0,0 +1,9 @@
+
+// Make a Calculator! using prompt(), and variables, make a program that does the following:
+
+var firstNumber = Number(prompt("What is your first number?: "));
+var secondNumber = Number(prompt("What is your second number?: "));
+alert("Your sum is: " + (firstNumber + secondNumber));
+alert("Your subtraction is: " + (firstNumber - secondNumber));
+alert("Your multiplication is: " + (firstNumber * secondNumber));
+alert("Your division is: " + (firstNumber / secondNumber));
