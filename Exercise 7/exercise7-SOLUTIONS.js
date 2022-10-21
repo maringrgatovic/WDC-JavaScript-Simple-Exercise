ad7d8f9964e3f1b70db0bb6030547d7bf8143b0b
maringrgatovic <56473997+maringrgatovic@users.noreply.github.com>
2022-10-21 16:05:25 +0200

    Add files via upload

---
+++ Exercise 7/exercise7-SOLUTIONS.js
@@ -0,0 +1,31 @@
+// Create an object and an array which we will use in our facebook exercise. 
+
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+var user = {
+	username: "Marin",
+	password: "password"
+}
+
+// 2. Create an array which contains the object you have made above and name the array "database"
+var database = [
+	{
+		username: "Marin",
+		password: "password"
+	}
+];
+
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+var newsfeed = [
+	{
+		username: "Luffy",
+		timeline: "So much meat!"
+	},
+	{
+		username: "Zoro",
+		timeline: "Getting tired of dull blades."
+	},
+	{
+		username: "Sanji",
+		timeline: "Fish fillets or pork chops?"
+	}
+];
